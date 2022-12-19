9f3ed0fb5d4e9b52e3e0c8db60eae0c6d2b75efa
Jaeyong Cho <jaeyong.cho.ece@ajou.ac.kr>
2022-12-19 15:44:41 +0900

    Add layout

--- layouts/layout-ide.py
+++ layouts/layout-ide.py
@@ -1,5 +1,5 @@
 gdb.execute("tui new-layout ide -horizontal {src 5 cmd 1} 3 {thread 1 backtrace 1 breakpoint 1 local 1 member 1} 2")        
-gdb.execute("define hookpost-up\n layout ide\n end")
-gdb.execute("define hookpost-down\n layout ide\n end")
+gdb.execute("tui new-layout ide1 -horizontal {src 5 cmd 1} 3 {breakpoint 1 local 1 member 1} 2")        
+gdb.execute("tui new-layout ide1 -horizontal {src 5 cmd 1} 3 {local 1 member 1} 2")        
 
 gdb.execute("tui new-layout ide-bt -horizontal {src 5 cmd 1} 3 {multi 1} 2")
